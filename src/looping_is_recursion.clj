55ce33de2fb9136638e2dc9ba60367304c71b9fa
Jesper Skovgaard Nielsen <jesper@runlevel0.dk>
2014-04-19 17:55:53 +0200

    last-elm and seq=

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -8,10 +8,17 @@
   (power-helper 1 base exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (cond
+    (empty? a-seq) nil
+    (= (count a-seq) 1) (first a-seq)
+    :else (recur (rest a-seq))))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (cond
+    (and (empty? seq1) (empty? seq2)) true
+    (not= (count seq1) (count seq2)) false
+    (not= (first seq1) (first seq2)) false
+    :else (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
   ":(")
