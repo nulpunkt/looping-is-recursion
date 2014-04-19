c5ebed264f532fafeb0c630c663033363dec559c
Jesper Skovgaard Nielsen <jesper@runlevel0.dk>
2014-04-19 21:41:00 +0200

    loooooooooooops

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -31,14 +31,41 @@
       :elso (recur (inc i) pred (rest a-seq)))))
 
 (defn avg [a-seq]
-  -1)
+  (loop [sum 0
+         i 0
+         a-seq a-seq]
+    (if (empty? a-seq)
+      (/ sum i)
+      (recur (+ (first a-seq) sum) (inc i) (rest a-seq)))))
 
 (defn parity [a-seq]
-  ":(")
+  (loop
+    [acc '()
+     a-seq a-seq]
+    (if (empty? a-seq)
+      acc
+      (let [elm (first a-seq)
+            trimmed (remove (partial = elm) a-seq)]
+        (if (odd? (- (count trimmed) (count a-seq)))
+          (recur (cons elm acc) trimmed)
+          (recur acc trimmed))))))
 
 (defn fast-fibo [n]
-  ":(")
+  (if (< n 2)
+    n
+    (loop [f1 0
+           f2 1
+           n n]
+      (if (zero? n)
+        f1
+        (recur f2 (+ f1 f2) (dec n))))))
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+  (loop [acc []
+         seen-so-far #{}
+         a-seq a-seq]
+    (let [current (first a-seq)]
+      (if (or (empty? a-seq) (contains? seen-so-far current))
+        acc
+        (recur (conj acc current) (conj seen-so-far current) (rest a-seq))))))
 
